736853dff9032f1aa9cd5eba13a5470fccf39c93
rextree <Rexy@Rexs-MBP-2.fritz.box>
2017-05-04 23:45:03 +1200

    Responsive Website
    The website is now suitable for any size of the screen on any devices.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
 $(document).ready(function () {
 
-    /* For the sticky navigation bar */
-    $('.js--aboutMe').waypoint(function (direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '70px;'
-    });
+	/* For the sticky navigation bar */
+	$('.js--aboutMe').waypoint(function (direction) {
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '70px;'
+	});
 
-    /* Animation on scroll */
-    $('.js--panelText').waypoint(function (direction) {
-        $('.js--panelText').addClass('animated fadeIn');
-    }, {
-        offset: '70%;'
+	/* Animation on scroll */
+	$('.js--panelText').waypoint(function (direction) {
+		$('.js--panelText').addClass('animated fadeIn');
+	}, {
+		offset: '70%;'
 
-    });
+	});
 
-    $('.js--nav--icon').click(function () {
-        var nav = $('.js--mainNav');
-        nav.toggle();
-    });
+	$('.js--nav--icon').click(function () {
+		var nav = $('.js--mainNav');
+		nav.toggle();
+	});
 
 
-    var leftPanel = $('.leftPanel');
-    $('.rightPanel').css({
-        height: leftPanel.height()
-    });
+	var leftPanel = $('.leftPanel');
+	$('.rightPanel').css({
+		height: leftPanel.height()
+	});
 
+	var hamburger = $(".hamburger");
+	hamburger.on("click", function () {
+		hamburger.toggleClass("is-active");
+		// Do something else, like open/close menu
+		$('.mainNav').slideToggle();
+	});
 
 });
 
@@ -37,29 +43,29 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+	var i;
+	var slides = document.getElementsByClassName("slides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = slides.length
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
 }
